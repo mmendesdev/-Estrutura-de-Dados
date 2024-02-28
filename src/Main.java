38c922e0c41ccda735ab7e2f951fcb872f0fa7eb
Matheus Mendes <matheus.mendes@somosicev.com>
2024-02-27 22:31:16 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Queue<Pacote> filaDePacotes = new ArrayDeque<>();
+        int numeroDePacotes = 10;
+        Scanner scannerDePacotes = new Scanner(System.in);
+
+        for(int i = 0; i < numeroDePacotes; i++){
+            System.out.println();
+        }
+        
+    }
+    
+}
---
+++ src/Pacote.java
@@ -0,0 +1,17 @@
+public class Pacote {
+
+    private int numeracao;
+
+    Pacote(int numeracao){
+        this.numeracao = numeracao; 
+    }
+
+    public int getNumeracao() {
+        return numeracao;
+    }
+
+    public void setNumeracao(int numeracao) {
+        this.numeracao = numeracao;
+    }
+    
+}
